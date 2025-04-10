9b6fd254fec111f243f7e4b7aaed15e6c88bb126
Nolan1324 <nkuza8@gmail.com>
2025-04-10 12:59:44 -0400

    Add higher-order logic

--- src/predicates.py
+++ src/predicates.py
@@ -3,8 +3,49 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, Generator, List, Optional, Tuple
+from typing import Dict, Generator, List, Literal, Optional, Tuple
 
+class MaybeCls():
+    def __and__(self, other):
+        if not self.__is_hol(other):
+            raise TypeError("Unsupported operand type(s) for and")
+        return {
+            True: Maybe,
+            False: False,
+            Maybe: Maybe,
+        }[other]
+    
+    def __or__(self, other):
+        if not self.__is_hol(other):
+            raise TypeError("Unsupported operand type(s) for or")
+        return {
+            True: True,
+            False: Maybe,
+            Maybe: Maybe,
+        }[other]
+    
+    def __invert__(self):
+        return Maybe
+    
+    def __rand__(self, other):
+        return self.__and__(other)
+    
+    def __ror__(self, other):
+        return self.__or__(other)
+    
+    def __repr__(self):
+        return 'Maybe'
+    
+    def __bool__(self):
+        raise ValueError('The truth value of Maybe is undefined')
+    
+    @staticmethod
+    def __is_hol(val):
+        return val == Maybe or isinstance(val, bool)
+
+Maybe = MaybeCls()
+
+HOL = Literal[True] | Literal[False] | MaybeCls
 
 class Operator(Enum):
     # EQ = "=="
@@ -24,7 +65,7 @@ class Predicate(ABC):
         pass
 
     @abstractmethod
-    def range_may_satisfy(self, ranges: Dict[str, Range]) -> bool:
+    def range_may_satisfy(self, ranges: Dict[str, Range]) -> HOL:
         pass
 
     @abstractmethod
@@ -46,13 +87,25 @@ class Atomic(Predicate):
             case Operator.LTE:
                 return x <= self.value
 
-    def range_may_satisfy(self, ranges: Dict[str, Range]) -> bool:
+    def range_may_satisfy(self, ranges: Dict[str, Range]) -> HOL:
         start, end = ranges[self.attr]
         match self.op:
             case Operator.GTE:
-                return end is None or end >= self.value
+                if end is None or end >= self.value:
+                    if start is not None and start >= self.value:
+                        return True
+                    else:
+                        return Maybe
+                else:
+                    return False
             case Operator.LTE:
-                return start is None or start <= self.value
+                if start is None or start <= self.value:
+                    if end is not None and end <= self.value:
+                        return True
+                    else:
+                        return Maybe
+                else:
+                    return False
             case _:
                 assert False
 
@@ -67,8 +120,11 @@ class And(Predicate):
     def evaluate(self, vals: Dict[str, int]) -> bool:
         return all(predicate.evaluate(vals) for predicate in self.predicates)
 
-    def range_may_satisfy(self, ranges: Dict[str, Range]) -> bool:
-        return all(predicate.range_may_satisfy(ranges) for predicate in self.predicates)
+    def range_may_satisfy(self, ranges: Dict[str, Range]) -> HOL:
+        val = True
+        for predicate in self.predicates:
+            val &= predicate.range_may_satisfy(ranges)
+        return val
 
     def atomics(self) -> Generator[Atomic, None, None]:
         for predicate in self.predicates:
@@ -82,8 +138,11 @@ class Or(Predicate):
     def evaluate(self, vals: Dict[str, int]) -> bool:
         return any(predicate.evaluate(vals) for predicate in self.predicates)
 
-    def evaluate_range(self, ranges: Dict[str, Range]) -> bool:
-        return any(predicate.range_may_satisfy(ranges) for predicate in self.predicates)
+    def range_may_satisfy(self, ranges: Dict[str, Range]) -> HOL:
+        val = False
+        for predicate in self.predicates:
+            val |= predicate.range_may_satisfy(ranges)
+        return val
 
     def atomics(self) -> Generator[Atomic, None, None]:
         for predicate in self.predicates:
