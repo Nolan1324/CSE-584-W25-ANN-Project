3469f21d5685b72c2c3634d7423561c6fc9801d4
Nolan1324 <nkuza8@gmail.com>
2025-04-12 01:23:33 -0400

    Add get partition option without tree

--- src/multirange_partitioner.py
+++ src/multirange_partitioner.py
@@ -10,7 +10,18 @@ class MultiRangePartitioner():
         self.partitions = get_partitions_from_tree(tree)
 
     def get_partition(self, vals: Dict[str, int]) -> str:
-        return self.tree.find_partition(vals)
+        if self.tree:
+            return self.tree.find_partition(vals)
+        else:
+            for partition_name, ranges in self.partitions.items():
+                for attr_name, val in vals.items():
+                    if attr_name not in ranges:
+                        continue
+                    start, end = ranges[attr_name]
+                    if (start is not None and val < start) or (end is not None and val > end):
+                        break
+                else:
+                    return partition_name
 
     def get_query_partitions(self, predicate: Predicate) -> Generator[str, None, None]:
         for name, ranges in self.partitions.items():
